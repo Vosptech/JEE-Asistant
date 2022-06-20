a01526f81fd3b7aa7a7c8afe1a4883be23835526
Vosptech <vosptech@gmail.com>
2022-06-20 23:36:04 +0530

    Second commit: Addded Signin with Google Option Date: 20/06/2022 Progress: 1] Email register and sign in without Verification 2]Added Sign in with Goggle option 3]Added Text view to main activity showing user details extracted from the google account. 4] Added Signout button in main activity.

--- app/src/main/java/com/example/jeeasistant/LoginActivity.kt
+++ app/src/main/java/com/example/jeeasistant/LoginActivity.kt
@@ -9,14 +9,19 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private val RC_SIGN_IN = 73
+    private var mGoogleSignInClient: GoogleSignInClient? = null
     private lateinit var lemail: TextView
     private lateinit var lpwd: TextView
     private lateinit var lbtn: Button
@@ -49,6 +54,7 @@ class LoginActivity : AppCompatActivity() {
                             baseContext, "Authentication failed.",
                             Toast.LENGTH_SHORT
                         ).show()
+                        updateUI(null)
 
                     }
                 }
@@ -56,19 +62,66 @@ class LoginActivity : AppCompatActivity() {
 
         }
 
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
 
-    }
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        sign_in_button.setOnClickListener {
+            signIn()
+        }
+    } //OnCreate function ends here
 
     private fun updateUI(user: FirebaseUser?) {
         val next = Intent(this, MainActivity::class.java)
         startActivity(next)
 
     }
-
     fun registerUser(view: View) {
         val registertheuser = Intent(this, emailRegisterActivity::class.java)
         startActivity(registertheuser)
 
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+
+        if(account!=null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun signIn() {
+        val intent = mGoogleSignInClient!!.signInIntent
+        startActivityForResult(intent, RC_SIGN_IN)
+    }
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account : GoogleSignInAccount? = task.getResult(ApiException::class.java)
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+            } catch (e: ApiException) {
+                // The ApiException status code indicates the detailed failure reason.
+                // Please refer to the GoogleSignInStatusCodes class reference for more information.
+
+            }
+        }
     }
 }
 
--- app/src/main/java/com/example/jeeasistant/MainActivity.kt
+++ app/src/main/java/com/example/jeeasistant/MainActivity.kt
@@ -1,23 +1,65 @@
 package com.example.jeeasistant
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import kotlin.io.print as print
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
+
+    private var mGoogleSignInClient : GoogleSignInClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+
+        if(acct != null) {
+            val personName = acct.displayName    //here the variable personName wil store the display name
+            person_name.text = personName
+            val personGivenName = acct.givenName
+            val personFamilyName = acct.familyName
+            val personEmail = acct.email
+            person_email.text = personEmail
+            val personId = acct.id
+            person_id.text = personId
+            val personPhoto: Uri? = acct.photoUrl
+        }
+
 
 
     }
+    fun signOut(){
+        mGoogleSignInClient?.signOut()?.addOnCompleteListener(this, object: OnCompleteListener<Void>{
+            override fun onComplete(p0: Task<Void>) {
+                Toast.makeText(this@MainActivity, "Signed Out", Toast.LENGTH_LONG).show()
+            }
+        })
 
-    fun nextscreen(view: View) {
-        val intent = Intent(this, emailRegisterActivity::class.java)
+        val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
+        finish()
     }
+
+    fun signOut(view: View) {
+        signOut()
+    }
+
+
 }
